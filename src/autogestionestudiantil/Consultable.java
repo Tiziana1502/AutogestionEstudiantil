75a39a1dba1c0fd8c28e4c1982ce975536f67ee4
benjaloza06 <benjaminloza06@gmail.com>
2026-05-11 03:45:39 -0300

    Creo la clase Incripcion materia y bonus

--- src/autogestionestudiantil/Consultable.java
+++ src/autogestionestudiantil/Consultable.java
@@ -2,7 +2,6 @@ package autogestionestudiantil;
 
 public interface Consultable {
 
-    // Método a implementar por la clase que use esta interfaz
-    // Muestra los datos principales del objeto
+  
     void mostrarResumen();
 }
